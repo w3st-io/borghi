711638931e46b118aec7880fe159ad69eeec6566
aleem ahmed <w3st.io2021@gmail.com>
2022-05-17 13:56:05 -0400

    Improved

--- client/src/components/nav/NavBar.vue
+++ client/src/components/nav/NavBar.vue
@@ -1,25 +1,31 @@
 <template>
-	<div class="w-100 fixed-top">
+	<div>
 		<!-- Left -->
-		<div class="mobile-nav-left py-4">
+		<div class="mobile-nav-left transition py-4">
 			<div class="ml-3">
-				<img :src="logoImg" alt="" style="max-width: 35px;">
+				<img
+					:src="logo.light"
+					class="transition"
+					style="max-width: 35px;"
+				>
 			</div>
 		</div>
 
 		<!-- Center -->
 		<div
-			class="mobile-nav py-4 transition text-center"
+			class="mobile-nav transition py-4 text-center"
 			:class="[navClass]"
 		>
-			<h4 class="logo m-0 text-spread">BORGHI FINE ART</h4>
-			<h6 class="m-0">{{ companyInfo.companyCaption1 }}</h6>
+			<h4 class="logo m-0 font-weight-bold text-spread">BORGHI FINE ART</h4>
+			<h6 class="m-0 font-weight-bold text-spread-sm small">
+				{{ companyInfo.companyCaption1 }}
+			</h6>
 		</div>
 
 		<!-- Right -->
 		<div class="mobile-nav-right py-4">
 			<div class="mr-3">
-				<BButton variant="dark" @click="toggle()">
+				<BButton variant="outline-light" class="border-0" @click="toggle()">
 					<MenuIcon />
 				</BButton>
 			</div>
@@ -38,7 +44,6 @@
 	import SideMenu from '@/components/nav/SideMenu'
 	import companyInfo from '@/defaults/companyInfo'
 	import buttons from '@/defaults/pageLinks'
-	import router from '@/router'
 
 	export default {
 		components: {
@@ -48,13 +53,19 @@
 
 		data() {
 			return {
-				logoImg: require('../../assets/logo.png'),
-				navClass: '',
 				companyInfo: companyInfo,
+
+				logo: {
+					dark: require('../../assets/logo.png'),
+					light: require('../../assets/logo-light.png'),
+				},
+
+				showDarkLogoImg: true,
+				navClass: 'bg-glass text-light',
 				buttons: buttons,
 				loggedIn: false,
 				decoded: {},
-				sideMenuOpen: false,router:router
+				sideMenuOpen: false,
 			}
 		},
 
@@ -64,9 +75,11 @@
 			handleScroll() {
 				if (window.scrollY > 0) {
 					this.navClass = 'bg-glass text-light'
+					this.showDarkLogoImg = true
 				}
 				else {
-					this.navClass = 'text-shadow'
+					this.navClass = 'bg-glass text-light'
+					this.showDarkLogoImg = false
 				}
 			},
 		},
@@ -111,11 +124,7 @@
 		backdrop-filter: blur(10px)!important;
 	}
 
-	.logo {
-		font-weight: bold;
-	}
-
 	.text-shadow {
-		text-shadow: 1px 1px #ffffff;
+		text-shadow: .5px 0px #000000;
 	}
 </style>
